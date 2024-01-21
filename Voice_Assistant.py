f9719a2a6109f97eb528bbb01b9ecf7ae3d5f0f0
Bhavyaj1209 <138478947+Bhavyaj1209@users.noreply.github.com>
2024-01-21 07:01:25 +0530

    Update Voice_Assistant.py

--- Voice_Assistant.py
+++ Voice_Assistant.py
@@ -2,29 +2,80 @@ import pyttsx3
 import speech_recognition as sr
 import webbrowser
 import datetime
-import pyscreenshot as ps
+import wikipedia
 from dotenv import load_dotenv
 import os
-from langchain.chat_models import ChatGooglePalm
-from langchain.schema import (
-    HumanMessage
-)
-import requests
 from newspaper import Article
+from langchain.chat_models import ChatGooglePalm
+from langchain.schema import HumanMessage
+import requests
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
+from tkinter import font
+from PIL import Image, ImageTk
+from threading import Thread
+import sys
+import pyjokes
+from googletrans import Translator
+import pyscreenshot as ps
 
 load_dotenv()
 chat = ChatGooglePalm(google_api_key=os.getenv("GOOGLE_API_KEY"))
 
+# Constants for custom styling
+BG_COLOR = "#D2C6E2"
+BUTTON_COLOR = "#F9F4F2"
+BUTTON_FONT = ("Arial", 14, "bold")
+BUTTON_FOREGROUND = "black"
+HEADING_FONT = ("white", 24, "bold")
+INSTRUCTION_FONT = ("Helvetica", 14)
+
+
+# --- classes ---
+
+class Redirect():
+    
+    def __init__(self, widget):
+        self.widget = widget
+
+    def write(self, text):
+        self.widget.insert('end', text)
+        #self.widget.see('end') # autoscroll
+    
+# --- main ---  
+
+
+def speak(audio):
+    engine = pyttsx3.init()
+    # getter method(gets the current value
+    # of engine property)
+    voices = engine.getProperty("voices")
+
+    # setter method .[0]=male voice and
+    # [1]=female voice in set Property.
+    engine.setProperty("voice", voices[0].id)
+
+    # Method for the speaking of the assistant
+    engine.say(audio)
+
+    # Blocks while processing all the currently
+    # queued commands
+    engine.runAndWait()
 
 # this method is for taking the commands
-# and recognizing the command from the speech_Recognition module we will use the recongizer method for recognizing
+# and recognizing the command from the
+# speech_Recognition module we will use
+# the recongizer method for recognizing
 def takeCommand():
     r = sr.Recognizer()
 
-    # from the speech_Recognition module we will use the Microphone module for listening the command
+    # from the speech_Recognition module
+    # we will use the Microphone module
+    # for listening the command
     with sr.Microphone() as source:
-        print("Listening")
-
+        print("Now Listening")
+        speak("Now Listening")
         # seconds of non-speaking audio before
         # a phrase is considered complete
         r.pause_threshold = 0.7
@@ -36,31 +87,62 @@ def takeCommand():
         # handling
         try:
             print("Recognizing")
-
+            
+            # for Listening the command in indian
+            # english we can also use 'hi-In'
+            # for hindi recognizing
             Query = r.recognize_google(audio, language="en-in")
             print("the command is printed=", Query)
 
         except Exception as e:
             print(e)
             print("Say that again sir")
+            speak("Say that again sir")
             return "None"
 
         return Query
 
-def speak(audio):
-    engine = pyttsx3.init()
-    # getter method(gets the current value of engine property)
-    voices = engine.getProperty("voices")
 
-    # setter method .[0]=male voice and
-    # [1]=female voice in set Property.
-    engine.setProperty("voice", voices[0].id)
 
-    # Method for the speaking of the assistant
-    engine.say(audio)
+entry = None
+stop_flag = False  # Define the stop_flag variable at the top of the script
 
-    # Blocks while processing all the currently queued commands
-    engine.runAndWait()
+
+def tellDay():
+    # This function is for telling the
+    # day of the week
+    day = datetime.datetime.today().weekday() + 1
+
+    # this line tells us about the number
+    # that will help us in telling the day
+    Day_dict = {
+        1: "Monday",
+        2: "Tuesday",
+        3: "Wednesday",
+        4: "Thursday",
+        5: "Friday",
+        6: "Saturday",
+        7: "Sunday",
+    }
+
+    if day in Day_dict.keys():
+        day_of_the_week = Day_dict[day]
+        print(day_of_the_week)
+        speak("The day is " + day_of_the_week)
+
+
+def tellTime():
+    # This method will give the time
+    time = str(datetime.datetime.now())
+
+    # the time will be displayed like
+    # this "2020-06-05 17:50:14.582630"
+    # nd then after slicing we can get time
+    print(time)
+    hour = time[11:13]
+    min = time[14:16]
+    speak("The time is sir" + hour + "Hours and" + min + "Minutes")
+
 
 def articleNarration():
     headers = {
@@ -109,56 +191,34 @@ def articleNarration():
 
     messages = [HumanMessage(content=prompt)]
     summary = chat(messages)
-    print(summary.content)
+    sum_content = summary.content
+    print(sum_content.replace("*", ""))
     speak(summary.content)
-
-def tellDay():
-    # This function is for telling the day of the week
-    day = datetime.datetime.today().weekday() + 1
-
-    # this line tells us about the number
-    # that will help us in telling the day
-    Day_dict = {
-        1: "Monday",
-        2: "Tuesday",
-        3: "Wednesday",
-        4: "Thursday",
-        5: "Friday",
-        6: "Saturday",
-        7: "Sunday",
-    }
-
-    if day in Day_dict.keys():
-        day_of_the_week = Day_dict[day]
-        print(day_of_the_week)
-        speak("The day is " + day_of_the_week)
-
-
-def tellTime():
-    # This method will give the time
-    time = str(datetime.datetime.now())
-
-    # the time will be displayed like
-    # this "2020-06-05 17:50:14.582630"
-    # And then after slicing we can get time
-    print(time)
-    hour = time[11:13]
-    min = time[14:16]
-    speak("The time is sir" + hour + "Hours and" + min + "Minutes")
 
 
 def Hello():
-    # This function is for when the assistant is called it will say hello and then take query
-    speak("hello sir I am your desktop assistant tell me how may I help you")
+    # This function is for when the assistant
+    # is called it will say hello and then
+    # take query
+    speak("hello, I am your friendly voice assistant Jarvis, integrated with AI. Tell me how may I help you")
 
 
 def Take_query():
-    # calling the Hello function for making it more interactive
+    # calling the Hello function for
+    # making it more interactive
     Hello()
+    global stop_flag
 
-    # This loop is infinite as it will take our queries continuously until and unless we do not say bye to exit or terminate the program
-    while True:
-        # taking the query and making it into lower case so that most of the times query matches and we get the perfect output
+    # This loop is infinite as it will take
+    # our queries continuously until and unless
+    # we do not say bye to exit or terminate
+    # the program
+    # while True:
+    while not stop_flag:
+        # taking the query and making it into
+        # lower case so that most of the times
+        # query matches and we get the perfect
+        # output
         query = takeCommand().lower()
 
         if "open google" in query:
@@ -166,9 +226,9 @@ def Take_query():
             webbrowser.open("www.google.com")
             continue
 
-        elif "show me some mr beast videos" in query:
+        elif "show me some videos" in query:
             speak("Opening videos")
-            webbrowser.open("www.youtube.com/@MrBeast")
+            webbrowser.open("www.youtube.com")
             continue
 
         elif "I want to read some tech news" in query:
@@ -181,21 +241,37 @@ def Take_query():
             webbrowser.open("https://www.google.com/maps/search/coffee+shop+near+me")
             continue
 
-        elif "which day it is" in query:
+        elif "where is" in query:
+            query = query.replace("where is", "")
+            location = query
+            speak("user asked to locate")
+            speak(location)
+            webbrowser.open(
+                "https://www.google.nl/maps/search/" + location.replace(" ", "+")
+            )
+            continue
+
+        elif "which day is it" in query:
             tellDay()
             continue
-
-        elif "tell me the time" in query:
-            tellTime()
-            continue
-
-        elif "tell me your name" in query:
-            speak("I am Jarvis. Your desktop Assistant")
 
         elif "take screenshot" in query:
             speak("Taking a screenshot")
             im = ps.grab()
             im.save("fullscreen.png")
+
+        elif "tell me the time" in query:
+            tellTime()
+            continue
+
+        elif "translate" in query:
+            query = query.replace("translate", "")
+            translator = Translator()
+            translation = query
+            trs = translator.translate(text=translation, dest="en", src="auto")
+            print(trs.text)
+            speak(trs.text)
+            continue
 
         elif "from wikipedia" in query:
             # if any one wants to have a information
@@ -208,18 +284,133 @@ def Take_query():
             # it also.
             result = wikipedia.summary(query, sentences=4)
             speak("According to wikipedia")
+            print(result)
             speak(result)
+
+        elif "tell me a joke" in query:
+            speak(pyjokes.get_joke(category='chuck'))
+
+        elif "tell me your name" in query:
+            speak("I am Jarvis. Your voice Assistant")
 
         elif "today's tech news" in query:
             articleNarration()
             continue
 
         # this will exit and terminate the program
-        elif "go back to sleep" in query:
-            speak("Bye")
+        elif "go back to sleep jarvis" in query:
+            speak("Bye Sir")
             exit()
 
 
-if __name__ == "__main__":
-    # main method for executing the functions
+def stop_voice_assistant():
+    global stop_flag
+    speak("Stopping the Voice Assistant.")
+    stop_flag = True
+
+
+def start_voice_assistant():
+    global stop_flag
     Take_query()
+    stop_flag = False  # Reset the flag to False when starting the voice assistant
+
+def main():
+    # Create the main GUI window
+    root = tk.Tk()
+    root.resizable(True, True)
+    root.title("Voice Assistant")
+    root.geometry("1366x768")
+    root.maxsize(1920, 1080)
+    root.minsize(900, 720)
+    root.configure(bg=BG_COLOR)
+
+    def on_button_click():
+        global stop_flag
+        if not stop_flag:
+            stop_flag = (
+                False  # Reset the flag to False when starting the voice assistant
+            )
+            Thread(target=start_voice_assistant).start()
+        else:
+            stop_voice_assistant()
+
+    highlightFont = font.Font(family='Helvetica', name='appHighlightFont', size=16, weight='bold')
+
+    # Load and set the background image
+    background_image = Image.open("abstract-equalizer.jpg")
+    background_photo = ImageTk.PhotoImage(background_image)
+    background_label = ttk.Label(root, image=background_photo)
+    background_label.place(x=0, y=0, relwidth=1, relheight=1)
+
+    f1 = ttk.Frame(root)
+    f1.pack(pady=100)  # Add some padding to the frame to center it vertically
+    def onClick(): 
+        tk.messagebox.showinfo("Instructions",  """Use these instructions to use the voice assistant
+Please make sure you are connected to internet and it is advised to use the application in full screen.
+
+1. Say 'open google': opens a web browser
+2. Say 'show me some videos': opens youtube
+3. Say 'I want to read a tech news': open gadgets360.com/news
+4. Say 'coffee shop near me': show all the coffee shop near you
+5. Say 'where is {place name}: give the location of that place
+6. Say 'which day is it': tell you the today's day
+7. Say 'tell me the time': tell you the current time
+8. Say 'from wikipedia {followed by your query}: search the result from wikipedia.com
+9. Say 'tell me your name': voice assistant tell you its name
+10. Say 'today's tech news': Summarize the top tech news around the gloab with the help of AI
+11. Say 'translate {followed by the language which you want to translate }: translate language to english. Currently it supports 107 languages.
+""") 
+    
+    # Create a Button 
+    button = tk.Button(root, text="Instructions", command=onClick, height=2, width=10) 
+    
+    # Set the position of button on the top of window. 
+    button.pack(side='top') 
+
+    # Heading
+    heading_label = ttk.Label(
+        root, text="Voice Assistant", font=highlightFont, background=None
+    )
+    heading_label.pack(pady=20)
+
+    global entry
+
+    # Instruction
+    instruction_label = ttk.Label(
+        root,
+        text="Click the button below to start the Voice Assistant.",
+        font=highlightFont,
+        background="white",
+    )
+    instruction_label.pack(pady=10)
+
+    # Create and place a button on the GUI
+    button = ttk.Button(
+        root,
+        text="Start Voice Assistant",
+        command=on_button_click,
+        style="VoiceAssistant.TButton",
+    )
+    button.pack(pady=20)
+
+    # Style the button
+    style = ttk.Style(root)
+    style.configure(
+        "VoiceAssistant.TButton",
+        font=highlightFont,
+        background=BUTTON_COLOR,
+        foreground=BUTTON_FOREGROUND,
+    )
+
+    text = tk.Text(root)
+    text.pack()
+    sys.stdout = Redirect(text)
+
+    # Run the GUI main loop
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    # main method for executing
+    # the functions
+    main()
